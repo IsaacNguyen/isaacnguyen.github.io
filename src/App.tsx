4c4bf0deff7deb0c6d92eec2cad4c8153664df23
IsaacNguyen <isaachugh33@gmail.com>
2024-05-08 01:53:12 -0700

    started!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 //import { useState } from 'react'
 import './App.css'
+import Taskbar from './Taskbar';
 
 function App() {
 
   return (
     <>
-      <div>
-        hello creamy shit poo
+      <div className ="bg-gradient-to-r from-cyan-500 to-blue-500">
+        <div className="fixed">
+          <Taskbar></Taskbar>
+        </div>
+          <div className = "h-screen">
+            <div className="container mx-auto text-center p-5">
+              hi i am goated
+            </div>
+          </div>
       </div>
     </>
   )
---
+++ src/Taskbar.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+
+function Taskbar() {
+
+  return (
+    <>
+      <div className="p-2 w-screen mx-0 text-center bg-transparent border-zinc-400 border-b">
+        <button className="hover:text-slate-100"> 
+            Projects
+        </button>  
+      </div>
+    </>
+  )
+}
+
+export default Taskbar;
