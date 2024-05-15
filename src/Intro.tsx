0f23bfdb7f513663230d6bd09deba9871a056445
Isaac Nguyen <isaachn@uci.edu>
2024-05-15 01:53:08 -0700

    better

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,12 +1,24 @@
-
+import './index.css'
 function Intro() {
 
     return (
       <>
-        <div className="p-2 h-lvh w-screen mx-0 text-center bg-transparent border-zinc-400 border-b">
-          <button className="hover:text-slate-100"> 
-              Projects
-          </button>  
+        <div className="flex h-96 w-screen mx-0 bg-transparent items-center justify-center ">
+          <div className = "fade-in-image flex items-center h-2/3 align-middle w-1/2">
+              <div className = "text-left pr-5">
+                <div className = "text-4xl">
+                  Hello! I am
+                </div>
+                <div className = "text-6xl pb-3">
+                  Isaac Nguyen
+                </div>
+                <div>
+                  I like coding and having lots of fun
+                </div>
+              </div>
+
+            <div> put your face right here</div>
+          </div>
         </div>
       </>
     )
