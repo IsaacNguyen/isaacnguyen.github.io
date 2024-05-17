9c7d311d8b8ed501a165211417c6823e2128499c
Isaac Nguyen <isaachn@uci.edu>
2024-05-17 00:43:16 -0700

    YAY

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -6,7 +6,7 @@ function Intro() {
     return (
       <>
         <div className="flex h-96 w-screen mx-0 bg-transparent items-center justify-center ">
-          <div className = "pb-8 border-b-2 flex items-center h-4/5 align-middle w-1/2">
+          <div className = "pb-8 border-b-2 flex items-center h-4/5 align-middle w-2/5">
               <div className = "fade-in-image pr-10">
                 <p className = "text-4xl">
                   Hello! I am
@@ -18,7 +18,7 @@ function Intro() {
                   I like coding and having lots of fun!
                 </p>
               </div>
-            <img src = {headShot} className=' fade-in-image  h-full rounded-full'></img>
+            <img src = {headShot} className=' fade-in-image h-full ml-auto rounded-full'></img>
           </div>
         </div>
       </>
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,17 +1,41 @@
 import './index.css'
+import { useState } from 'react';
 
-function Projects() {
+function Projects(props) {
+  const [showSection, updateSection] = useState('coding');
 
+  const toggleSectionVisibility = (section: string) => {
+    updateSection(section);
+  };
     return (
       <>
-        <div className="fade-in-image flex h-96 w-screen mx-0 justify-center ">
-            <div className='flex w-2/3 justify-center'>
-              <div className='flex w-1/2 h-1/6 top-0 justify-between text-2xl'>
-                  <button className='pl-10 hover:underline underline-offset-1'>coding</button>
-                  <button className='pr-10 hover:underline underline-offset-1'>hobbies</button>
-              </div>
-            </div>   
+        <div className="fade-in-image flex h-96 w-screen mx-0 justify-center">
+            <div className='flex w-2/3 flex-col items-center'>
+              <div className='flex w-1/2 h-1/6 top-0 justify-between text-2xl pb-10 '>
+                  <button className='pl-10 hover:underline underline-offset-1'
+                    onClick ={() => toggleSectionVisibility('coding')}
+                  >coding</button>
+                  <button className='pr-10 hover:underline underline-offset-1'
+                    onClick ={() => toggleSectionVisibility('hobbies')}
+                    >hobbies</button>
 
+                 
+              </div>
+              {showSection == 'coding' && (
+                    <div className='grid grid-cols-2 gap-4 w-full h-full fade-in-image'>
+                      <div className='flex w-1/2 h-50 bg-black'>
+                        heyy
+                      </div>
+                      <div> heyyy </div>
+                      <div> heyyy </div>
+                      <div> heyyy </div>
+                      <div> heyyy </div>
+                    </div>
+
+                  )}
+
+            </div>   
+           
           </div>
      
       </>
