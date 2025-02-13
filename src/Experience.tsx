68611319094c128a7dfd3582a4befc60a2aa0167
IsaacNguyen <isaachugh33@gmail.com>
2025-02-13 11:35:54 -0800

    fixed

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -20,23 +20,14 @@ function Experience() {
                 </div>
                 <div className='flex flex-col gap-8 w-full'>
         
-                    <ExperienceTab
-                      logo={atomaLogo}
-                      image={atoma}
-                      role="Software Engineer Intern"
-                      name="Atoma Media"
-                      description="Developing an end-to-end website for a finance startup that uses AI to gather information about financial articles to democratize financial information."
-                      timeframe="July 2024 - Present"
-                      link = 'https://www.joinatoma.com/'
-                      rounded={false}
-                      />
+              
 
                     <ExperienceTab
                       logo={ctcLogo}
                       image={ctc}
                       role="Full-Stack Developer"
                       name="Commit the Change"
-                      description="Producing a digital case management tool to improve efficiency of aiding homeless women/children for a nonprofit organization called Colette&apos;s Children Home."
+                      description="Producing a digital case management tool to improve efficiency of aiding 7,000+ homeless women/children for a nonprofit organization called Colette&apos;s Children Home."
                       timeframe="October 2024 - Present"
                       link = 'https://ctc-uci.com/'
                       rounded={false}
@@ -45,9 +36,9 @@ function Experience() {
                     <ExperienceTab
                       logo={icsscLogo}
                       image={antalmanac}
-                      role="Software Developer"
+                      role="Software Development Engineer"
                       name="AntAlmanac - ICSSC Projects Committee"
-                      description="Contributing to the open source, student-run project named AntAlmanac, a schedule planning and course exploration tool for UCI students."
+                      description="Contributing to the open source, student-run project named AntAlmanac, a schedule planning and course exploration tool for 8,000+ UCI students."
                       timeframe="November 2024 - Present"
                       link = 'https://antalmanac.com/'
                       rounded={true}
@@ -62,6 +53,17 @@ function Experience() {
                       timeframe="October 2024 - Present"
                       link = 'https://www.genaied.org/'
                       rounded={true}
+                      />
+
+                      <ExperienceTab
+                      logo={atomaLogo}
+                      image={atoma}
+                      role="Software Engineering Intern"
+                      name="Atoma Media"
+                      description="Developing an end-to-end website for a finance startup that uses AI to gather information about financial articles to democratize financial information."
+                      timeframe="July 2024 - February 2025"
+                      link = 'https://www.joinatoma.com/'
+                      rounded={false}
                       />
 
                 </div>
